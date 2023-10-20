65b53859358dab8908908f26a064931cef555823
DisseminationNI <90850128+DisseminationNI@users.noreply.github.com>
2023-10-20 20:54:51 +0100

    Update map.library.js

--- js/map.library.js
+++ js/map.library.js
@@ -10,7 +10,13 @@ pxWidget.map.callback = {};
 pxWidget.map.geojson = [];
 pxWidget.map.jsonstat = [];
 pxWidget.map.values = [];
-
+pxWidget.map2 = {};
+pxWidget.map2.metadata = {};
+pxWidget.map2.ajax = {};
+pxWidget.map2.callback = {};
+pxWidget.map2.geojson = [];
+pxWidget.map2.jsonstat = [];
+pxWidget.map2.values = [];
 
 /**
  * Draw a pxWidget Chart
