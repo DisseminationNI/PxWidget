c1902564f9badaab57b1272f06e64aa0f5228553
DisseminationNI <90850128+DisseminationNI@users.noreply.github.com>
2024-05-07 20:29:02 +0100

    Update map.library.js

--- js/map.library.js
+++ js/map.library.js
@@ -295,7 +295,7 @@ The parent outer function must be async
     }
     var dateUpdated = "";
     if (pxWidget.map.jsonstat[id].updated) {
-        dateUpdated = pxWidget.moment(pxWidget.map.jsonstat[id].updated, 'YYYY-MM-DDTHH:mm:ss').format('MMMM DD, YYYY') + " " + pxWidget.moment(pxWidget.map.jsonstat[id].updated, 'YYYY-MM-DDTHH:mm:ss').format('HH:mm:ss') + " UTC";
+                dateUpdated = pxWidget.moment(pxWidget.map.jsonstat[id].updated, 'YYYY-MM-DDTHH:mm:ss').format('DD MMMM, YYYY');
     }
     var localAttribution = "<br>" + dateUpdated;
     localAttribution += pxWidget.draw.params[id].link ? " <a target='_blank' href='" + pxWidget.draw.params[id].link + "'>" + pxWidget.draw.params[id].link + "</a> <br>" : "<br>";
