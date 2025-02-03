df1f8277c0d35da19c5347345ec6256c8dc1d3f9
DisseminationNI <90850128+DisseminationNI@users.noreply.github.com>
2025-02-03 14:11:15 +0000

    Update chart.library.js

--- js/chart.library.js
+++ js/chart.library.js
@@ -76,7 +76,7 @@ pxWidget.chart.draw = function (id) {
                 ctx.textBaseline = "bottom";
                 ctx.fillStyle = "#666";
                 ctx.font = "14px Arial";
-                var dateUpdated = pxWidget.moment(chart.options.updated, 'YYYY-MM-DDTHH:mm:ss').format('MMMM DD, YYYY') + " " + pxWidget.moment(chart.options.updated, 'YYYY-MM-DDTHH:mm:ss').format('HH:mm:ss') + " UTC";
+               var dateUpdated = " Updated" + " "+ pxWidget.moment(chart.options.updated, 'YYYY-MM-DDTHH:mm:ss').format('DD MMMM, YYYY');
                 ctx.fillText(dateUpdated, 0, chart.canvas.clientHeight);
             }
             ctx.save();
